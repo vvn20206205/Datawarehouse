7c132f601b675fd36db88c27508a5b3354d7d328
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-03-06 03:36:01 +0700

    update 'Tuan1 copy.tex', Tuan1.tex and latex.py

--- contents/tools/latex.py
+++ contents/tools/latex.py
@@ -5,18 +5,46 @@ import glob
 # # from PIL import Image, ImageDraw, ImageFont
 # # #  pip install  pyperclip
 # # import pyperclip
- 
-def format(file):
-    print("🚀 Giá trị của file:", file)
+
+
+def format(path):
+    with open(path, 'r', encoding="utf-8") as file:
+        contents = file.read()
+
+
+    while ' ,' in contents:
+        contents = contents.replace(' ,', ',')
+    contents = contents.replace(',', ', ')
+
+    while '  ' in contents:
+        contents = contents.replace('  ', ' ')
+        
+    while '( ' in contents:
+        contents = contents.replace('( ', '(')
+    while ' )' in contents:
+        contents = contents.replace(' )', ')')
+    while '[ ' in contents:
+        contents = contents.replace('[ ', '[')
+    while ' ]' in contents:
+        contents = contents.replace(' ]', ']')
+    while '{ ' in contents:
+        contents = contents.replace('{ ', '{')
+    while ' }' in contents:
+        contents = contents.replace(' }', '}')
+
+    contents = '\n'.join(line.strip() for line in contents.split('\n'))
+    while "\n\n\n" in contents:
+        contents = contents.replace("\n\n\n", "\n\n")
+    contents = contents.lstrip('\n')
+    contents = contents.rstrip('\n')
+    with open(path, 'w', encoding="utf-8") as file:
+        file.write(contents)
+
+
+
 
 
 root = r"../../"
 tex_files = glob.glob(os.path.join(root, "**/*.tex"), recursive=True)
 for i in tex_files:
-
     format(i)
-
- 
-
-        # with open(path, 'r', encoding="utf-8") as file:
-        #     contents = file.read()
